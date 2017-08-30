c8fd65e83382dcaad7fe344adb17d9479457952d
John Doe <john@sample.com>
2017-08-30 23:26:46 +0530

    json file config

--- src/test/java/tests/SuruseLogin/Tests.java
+++ src/test/java/tests/SuruseLogin/Tests.java
@@ -1,4 +1,28 @@
 package tests.SuruseLogin;
 
-public class Tests {
+
+import config.SeleniumConfig.BrowserConfig;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Tests extends BrowserConfig {
+    @BeforeTest
+    public void login() {
+
+        performAction.openURL();
+        performAction.pause(5);
+        performAction.sendKeys("SuruseLogin", "userName");
+        performAction.sendKeys("SuruseLogin", "password");
+        performAction.click("SuruseLogin", "logIn");
+        performAction.pause(5);
+    }
+
+    @Test
+    public void createStream() {
+        performAction.pause(5);
+        performAction.click("SuruseLoging", "createStream");
+
+    }
+
+
 }
